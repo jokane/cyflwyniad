44224fab3a8edf16415ccb66fdd05c8c57ed8267
Jason O'Kane <jokane@cse.sc.edu>
2016-09-28 12:20:44 -0400

    Test for unclosed tag.

--- test_parser.py
+++ test_parser.py
@@ -1,5 +1,14 @@
-import cyflwyniad
+import pytest
+from cyflwyniad import *
 
 def test_unclosed_tag():
-  assert(True)
+  doc = Document(True)
+  with pytest.raises(Exception) as e:
+    doc.process_cy_string(
+      """
+        [slides]
+        !class{
+      """
+    )
 
+  
